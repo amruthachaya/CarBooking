99635fbb55efa3a1474d554090a2ab4e5f166a43
vk <vinay.tm@otocapital.in>
2023-07-14 18:28:20 +0530

    gos tracker

--- home/ser.py
+++ home/ser.py
@@ -4,16 +4,16 @@ from home.models import Tracking
 
 
 class GpsTracker(serializers.ModelSerializer):
-    device_id = serializers.IntegerField()
-    timestamp = serializers.IntegerField()
-    lat = serializers.FloatField()
-    lon = serializers.FloatField()
-    speed = serializers.FloatField()
-    bearing = serializers.FloatField()
-    altitude = serializers.FloatField()
-    accuracy = serializers.FloatField()
-    batt = serializers.FloatField()
-    charge = serializers.BooleanField()
+    device_id = serializers.IntegerField(default=1)
+    timestamp = serializers.IntegerField(default=111111)
+    lat = serializers.FloatField(default=0.0)
+    lon = serializers.FloatField(default=0.0)
+    speed = serializers.FloatField(default=0.0)
+    bearing = serializers.FloatField(default=0.0)
+    altitude = serializers.FloatField(default=0.0)
+    accuracy = serializers.FloatField(default=0.0)
+    batt = serializers.FloatField(default=0.0)
+    charge = serializers.BooleanField(default=False)
 
     class Meta:
         model = Tracking
